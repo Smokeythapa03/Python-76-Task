1bb6783527490ba5b3f12870850ce7c0be6a6443
Your Name <you@example.com>
2023-11-02 01:17:31 +0600

    first commit

---
+++ task2 .py
@@ -0,0 +1,41 @@
+#task1
+print("Hello")
+#task2
+print("Hi How are You")
+#Variable
+#task 9
+age = 18
+print(age)
+name = "dorji"
+print(name)
+#task12
+age = 18
+print("hi,your age is ",age)
+#task13
+#the comment line will not be executed
+#task16
+x = input ()
+print("Input is ",x)
+#task17
+x = input ("Enter Your Name: ")
+print("UserName",x)
+#task19
+x = input("Enter Your Name :")
+print("userName",x)
+y = input("Enter Your age :")
+print("userAge",y)
+print("HI",x,"your age is",y)
+#Conditionals
+#task20
+number1 = 1
+number2 = 2
+number1 == number2
+result = number1 == number2
+print("The output is",result)
+#task21
+number1 = 1
+number2 = 1
+number1 == number2
+result = number1 == number2
+print("The output is",result)
+
